59c0d96ed6b459eb7ba34af5a9b0d3792685f524
Tsahi kamar <tsahikamar@gmail.com>
2021-06-05 23:16:50 +0300

    commit

--- src/Server/Routes/general.js
+++ src/Server/Routes/general.js
@@ -1,3 +1,4 @@
+//On Server directory -> node server.js
 
 const express = require('express');
 const router = express.Router();
@@ -6,46 +7,26 @@ const mongo = require('mongodb');
 var ObjectID = require('mongodb').ObjectID;
 var dbo;   
 const uri = 'mongodb+srv://dbUser:dbUserPassword@cluster0.ezk2m.mongodb.net/HapoalimAppDB?retryWrites=true&w=majority';
-//const uri = 'mongodb+srv://' +  process.env.MONGO_USER_NAME + ':' + process.env.MONGO_ATLAS_PW + '@cluster0.ezk2m.mongodb.net/' + process.env.MONGO_PROJECT_NAME + '?retryWrites=true&w=majority';
-  
-    //1
-    //const client = new MongoClient(uri,{ useUnifiedTopology: true} );
-       
+         
 var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(uri,function(err,db) {
+MongoClient.connect(uri,{ useUnifiedTopology: true},function(err,db) {
 if (err) throw err;
-
-//orig temp remark db.close();
-        
+   
 dbo = db.db("HapoalimDB");    
 
 });    
 
-//const Customers = require('../Models/customers');
-
-
-//Find All Customers 
 //http://localhost:3000/general/customers
 router.get('/customers/',(req,res,next) => {    
         dbo.collection("customers").find({}).toArray(function(err, result) {
         if (err) throw err;
             console.log(result);
- //dbo.close();            
             res.status(200).json({
                 customers: result
              });         
     });    
-});
-    
-
+});    
   
-//  http://localhost:3000/general/customers/update
-// {
-//     "_id": "60bb2521473335024d4db03d",
-//     "_v":221,
-//     "_t":"tttyy"
-//   }
-
 //id 571fd15840c4ed0d240d7d6c
 router.post("/customers/update/",(req,res,next) => {
     
@@ -67,7 +48,6 @@ router.post("/customers/update/",(req,res,next) => {
     dbo.collection("customers").update(where, values, function(err, res) {
    
      if (err) throw err;
-//db.close();
      });
     
      return res.status(200).json({
@@ -78,13 +58,11 @@ router.post("/customers/update/",(req,res,next) => {
 //http://localhost:3000/general/customers/60bb2521473335024d4db03d
 router.get('/customers/:id',(req,res,next) => {  
 
-    //console.log('id:' + id);
     const id = req.params.id;
 
     dbo.collection("customers").findOne({"_id" : ObjectID(id)}, function(err, result) {
         if (err) throw err;
         console.log(result);
-//db.close();
         res.status(200).json({
             _id : result._id,
             v : result._v,
@@ -93,16 +71,12 @@ router.get('/customers/:id',(req,res,next) => {
 });    
   
 });
-  
-
 
 //http://localhost:3000/general/customers/balance
 router.get('/balances/',(req,res,next) => {    
     dbo.collection("balances").find({}).toArray(function(err, result) {
     if (err) throw err;
-        console.log('balances:' + result);
-//dbo.close();
-        
+    console.log('balances:' + result);
     res.status(200).json({
        balances: result
     });        
@@ -114,9 +88,7 @@ router.get('/balances/',(req,res,next) => {
 router.get('/deposits/',(req,res,next) => {    
     dbo.collection("deposits").find({}).toArray(function(err, result) {
     if (err) throw err;
-    console.log('deposits:' + result);
- //dbo.close();
-            
+    console.log('deposits:' + result);           
     res.status(200).json({
        deposits: result
     });        
@@ -128,9 +100,7 @@ router.get('/deposits/',(req,res,next) => {
 router.get('/withdrawls/',(req,res,next) => {    
     dbo.collection("withdrawls").find({}).toArray(function(err, result) {
     if (err) throw err;
-        console.log(result);
-//dbo.close();
-        
+    console.log(result);        
     res.status(200).json({
        withdrawls: result
     });        
@@ -142,10 +112,8 @@ router.get('/withdrawls/',(req,res,next) => {
 router.get('/euro/',(req,res,next) => {    
     dbo.collection("euro").find({}).toArray(function(err, result) {
     if (err) throw err;
-        console.log(result);
-//dbo.close();
-        
-    res.status(200).json({
+       console.log(result);        
+       res.status(200).json({
        euro: result
     });        
   });    
